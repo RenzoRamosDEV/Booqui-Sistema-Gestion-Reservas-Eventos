19eac865a88e48dacc86131b3d3972d891124e1e
Renzo Ramos <renzoramosivan@gmail.com>
2026-05-30 18:35:24 +0200

    fix(config): update base path for deployment in Vite configuration

--- front-demo/vite.config.js
+++ front-demo/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/Booqui-Sistema-Gestion-Reservas-Eventos/',
   plugins: [react()],
 })
